0ccebe4bc022fd28ebc0abf69490f1b53cc1b930
Tim de Pater <git@trafex.nl>
2019-01-16 10:15:35 +0100

    Revert the Nefit Easy component upgrade

--- custom_components/climate/nefit.py
+++ custom_components/climate/nefit.py
@@ -1,37 +1,39 @@
 """
-ORIGINAL: https://github.com/marconfus/ha-nefit-ng/blob/master/nefit.py
+Nefit thermostat.
+Tested with Junkers CT100
 
-Support for Bosch home thermostats.
-For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/xxxxxx/
+Based on nefit-client-python
+https://github.com/patvdleer/nefit-client-python
 """
 
-REQUIREMENTS = ['aionefit==0.1']
+REQUIREMENTS = ['nefit-client']
 
 import logging
 from datetime import timedelta
+import math
+#import asyncio
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
-import asyncio
-import concurrent
 
-from homeassistant.components.climate import (ClimateDevice, PLATFORM_SCHEMA,
-    STATE_AUTO, STATE_MANUAL,
-    SUPPORT_TARGET_TEMPERATURE, SUPPORT_OPERATION_MODE)
+from homeassistant.components.climate import ( ClimateDevice, PLATFORM_SCHEMA,
+    STATE_AUTO, STATE_HEAT, STATE_IDLE,
+    SUPPORT_TARGET_TEMPERATURE, SUPPORT_OPERATION_MODE )
 from homeassistant.const import TEMP_CELSIUS, ATTR_TEMPERATURE
 from homeassistant.const import STATE_UNKNOWN, EVENT_HOMEASSISTANT_STOP
+
+from nefit import NefitClient
 
 _LOGGER = logging.getLogger(__name__)
 
 SUPPORT_FLAGS = (SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE)
 
-OPERATION_MANUAL = "manual"
-OPERATION_AUTO = "auto"
-
 CONF_NAME = "name"
 CONF_SERIAL = "serial"
 CONF_ACCESSKEY = "accesskey"
 CONF_PASSWORD = "password"
+
+STATE_HOTWATER = "hot water"
+STATE_MANUAL = "manual"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_NAME): cv.string,
@@ -40,155 +42,174 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_PASSWORD): cv.string
 })
 
-async def async_setup_platform(hass, config, async_add_entities,
-                               discovery_info=None):
-
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Setup the Nefit thermostat."""
     name = config.get(CONF_NAME)
     serial = config.get(CONF_SERIAL)
     accesskey = config.get(CONF_ACCESSKEY)
     password = config.get(CONF_PASSWORD)
 
-    _LOGGER.debug("Creating device")
-    device = NefitThermostat(hass, name, serial, accesskey, password)
-    _LOGGER.debug("Waiting for connected event")
+    add_devices([NefitThermostat(
+        hass, name, serial, accesskey, password)], True)
 
-    try:
-        await device._client.xmppclient.connected_event.wait()
-    except concurrent.futures._base.TimeoutError:
-        hass.services.call("persistent_notification", "create",
-                           {'message': 'Could not connect to Bosch cloud.',
-                           'title': 'Nefit error',
-                           'notification_id': 'nefit_logon_error'})
-
-    _LOGGER.debug("Device is ready")
-    async_add_entities([device], True)
 
 class NefitThermostat(ClimateDevice):
     """Representation of a NefitThermostat device."""
 
     def __init__(self, hass, name, serial, accesskey, password):
-        from aionefit import NefitCore
         """Initialize the thermostat."""
         self.hass = hass
         self._name = name
-
-        self._online = False
+        self._serial = serial
+        self._accesskey = accesskey
+        self._password = password
         self._unit_of_measurement = TEMP_CELSIUS
-        self._uistatus = None
+        self._data = {}
         self._attributes = {}
-        self._operation_list = [OPERATION_MANUAL, OPERATION_AUTO]
+        self._attributes["connection_error_count"] = 0
+        self.override_target_temp = False
+        self.new_target_temp = 0
 
-        self._client = NefitCore(serial_number=serial,
-                       access_key=accesskey,
-                       password=password,
-                       message_callback=self.parse_message)
+        _LOGGER.debug("Constructor for {} called.".format(self._name))
 
-        _LOGGER.debug("adding stop listener")
-        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP,
-                                   self._shutdown)
+        hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP,
+                             self._shutdown)
 
-    @property
-    def supported_features(self):
-        """Return the list of supported features.
-        """
-        return SUPPORT_FLAGS
+        self._client = NefitClient(serial_number=self._serial,
+                             access_key=self._accesskey,
+                             password=self._password)
+        self._client.connect()
 
-    def parse_message(self, data):
-        """Message received callback function for the XMPP client.
-        """
-        _LOGGER.debug("parse_message callback called")
-        if not 'id' in data:
-            _LOGGER.error("Unknown response received.")
-            return
 
-        if data['id'] == '/ecus/rrc/uiStatus':
-            _LOGGER.debug('Message type uiStatus')
-            self._uistatus = data['value']
-
-    async def async_update(self):
-        """Get latest data
-        """
-        _LOGGER.debug("async_update called")
-        self._client.get('/ecus/rrc/uiStatus')
-
-        await asyncio.wait_for(self._client.xmppclient.message_event.wait(), timeout=10.0)
-        self._client.xmppclient.message_event.clear()
-        _LOGGER.debug("async_update finished")
 
     @property
     def name(self):
-        """Return the name of the ClimateDevice.
-        """
+        """Return the name of the ClimateDevice."""
         return self._name
 
     @property
     def temperature_unit(self):
-        """Return the unit of measurement.
-        """
+        """Return the unit of measurement."""
         return self._unit_of_measurement
-
-    @property
-    def current_temperature(self):
-        """Return the current temperature.
-        """
-        if not self._uistatus:
-            return STATE_UNKNOWN
-
-        iht = float(self._uistatus.get('IHT'))
-        _LOGGER.debug('current_temperature=%s', iht)
-        return iht
-
-    @property
-    def target_temperature(self):
-        if not self._uistatus:
-            return STATE_UNKNOWN
-
-        tot = float(self._uistatus.get('TOT'))
-        _LOGGER.debug('target_temperature=%s', tot)
-        return tot
 
     @property
     def operation_list(self):
         """List of available operation modes."""
         return [STATE_AUTO, STATE_MANUAL]
 
+    def update(self):
+        """Get latest data"""
+        _LOGGER.debug("update called.")
+        errors = []
+
+        try:
+            data = self._client.get_status()
+            _LOGGER.debug("update finished. result={}".format(data))
+
+            if type(data) is dict and "user mode" in data:
+                self._attributes["connection_state"] = "ok"
+            else:
+                self._attributes["connection_state"] = "error"
+                self._attributes["connection_error_count"] += self._attributes["connection_error_count"]
+
+            if data:
+                self._data = data
+
+            self._attributes["boiler_indicator"] = self._data.get("boiler indicator")
+            self._attributes["control"] = self._data.get("control")
+
+
+            r = self._client.get_year_total()
+            self._attributes["year_total"] = r.get("value")
+            self._attributes["year_total_unit_of_measure"] = r.get("unitOfMeasure")
+
+            r = self._client.get("/ecus/rrc/userprogram/activeprogram")
+            self._attributes["active_program"] = r.get("value")
+
+            r = self._client.get("/ecus/rrc/dayassunday/day10/active")
+            self._attributes["today_as_sunday"] = (r.get("value") == "on")
+
+            r = self._client.get("/ecus/rrc/dayassunday/day11/active")
+            self._attributes["tomorrow_as_sunday"] = (r.get("value") == "on")
+
+            r = self._client.get("/system/appliance/systemPressure")
+            self._attributes["system_pressure"] = r.get("value")
+
+            r = self._client.get("/heatingCircuits/hc1/actualSupplyTemperature")
+            self._attributes["supply_temp"] = r.get("value")
+
+            r = self._client.get("/system/sensors/temperatures/outdoor_t1")
+            self._attributes["outside_temp"] = r.get("value")
+        except:
+            _LOGGER.warning('Nefit api returned invalid data')
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        _LOGGER.debug("current_temperature called.")
+
+        return self._data.get('in house temp', None)
+
     @property
     def current_operation(self):
-        if not self._uistatus:
+        state = self._data.get("boiler indicator")
+        if state == 'central heating':
+            return STATE_HEAT
+        elif state == 'hot water':
+            return STATE_HOTWATER
+        elif state == 'off':
+            return STATE_IDLE
+        else:
             return None
 
-        if self._uistatus['UMD'] == 'manual':
-            return OPERATION_MANUAL
-        elif self._uistatus['UMD'] == 'clock':
-            return OPERATION_AUTO
+    @property
+    def target_temperature(self):
+
+        #update happens too fast after setting new target, so value is not changed on server yet.
+        #assume for this first update that the set target was succesful
+        if self.override_target_temp:
+            self._target_temperature = self.new_target_temp
+            self.override_target_temp = False
+        else:
+            self._target_temperature = self._data.get('temp setpoint', None)
+
+        return self._target_temperature
+
+    def set_temperature(self, **kwargs):
+        """Set new target temperature."""
+        try:
+            temperature = kwargs.get(ATTR_TEMPERATURE)
+            _LOGGER.debug("set_temperature called (temperature={}).".format(temperature))
+
+            if temperature is None:
+                return None
+
+            self._client.set_temperature(temperature)
+
+            self.override_target_temp = True
+            self.new_target_temp = temperature
+
+        except:
+            _LOGGER.error("Error setting target temperature")
+
+    def set_operation_mode(self, operation_mode):
+        """Set new target operation mode."""
+        _LOGGER.debug("set_operation_mode called mode={}.".format(operation_mode))
+        if operation_mode == "manual":
+            self._client.put('/heatingCircuits/hc1/usermode', {'value': 'manual'})
+        else:
+            self._client.put('/heatingCircuits/hc1/usermode', {'value': 'clock'})
 
     @property
     def device_state_attributes(self):
         """Return the device specific state attributes."""
 
-        return self._uistatus
+        return self._attributes
 
-    async def async_set_operation_mode(self, operation_mode):
-        """Set new target operation mode."""
-        _LOGGER.debug("set_operation_mode called mode={}.".format(operation_mode))
-        if operation_mode == "manual":
-            new_mode = "manual"
-        else:
-            new_mode = "clock"
-
-        self._client.set_usermode(new_mode)
-        await asyncio.wait_for(self._client.xmppclient.message_event.wait(), timeout=10.0)
-        self._client.xmppclient.message_event.clear()
-        self._uistatus['UMD'] = new_mode
-
-    async def async_set_temperature(self, **kwargs):
-        """Set new target temperature."""
-        temperature = kwargs.get(ATTR_TEMPERATURE)
-        _LOGGER.debug("set_temperature called (temperature={}).".format(temperature))
-        self._client.set_temperature(temperature)
-        await asyncio.wait_for(self._client.xmppclient.message_event.wait(), timeout=10.0)
-        self._client.xmppclient.message_event.clear()
-        self._uistatus['TOT'] = temperature
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return SUPPORT_FLAGS
 
     def _shutdown(self, event):
         _LOGGER.debug("shutdown")
