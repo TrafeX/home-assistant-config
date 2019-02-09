d3a6fe847a96103eea8ccfcb78dcdc58e74af459
Tim de Pater <git@trafex.nl>
2019-02-09 08:16:17 +0100

    Remove installing dependencies, the container already exists

--- test-config.sh
+++ test-config.sh
@@ -1,1 +1,1 @@
-docker exec -ti home-assistant bash -c 'pip3 install --no-cache-dir pyaes ; pip3 install --no-cache-dir nefit-client ; python -m homeassistant --script check_config -c /config'
+docker exec -ti home-assistant bash -c 'python -m homeassistant --script check_config -c /config'
