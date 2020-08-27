921313cc0fe51c3c8f8e1e2ff3754ce64e1a045b
Tim de Pater <git@trafex.nl>
2020-08-27 21:55:18 +0200

    Replace media player with new name

--- scp-to-prod.sh
+++ scp-to-prod.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-scp -r test-config.sh configuration.yaml secrets.yaml hass_sa.json ui-lovelace.yaml lovelace-*.yaml www/ configuration/ custom_components/ ha-server:/data/homeassistant
+scp -r test-config.sh configuration.yaml secrets.yaml hass_sa.json ui-lovelace.yaml www/ configuration/ custom_components/ ha-server:/data/homeassistant
